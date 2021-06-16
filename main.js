5521eeed71395be5e6aa17cead805209b93e9cb4
robertnebel <robertnebel0015@gmail.com>
2021-06-16 18:10:48 +0530

    commit 10 omg wt??!!

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css';
 import * as THREE from './node_modules/three';
-import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls';
-import { StaticReadUsage } from 'three';
+// import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls';
+// import { StaticReadUsage } from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
